590d526bc69f0d8d07d75d5aa7b6e849032d007b
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-21 12:25:42 +0300

    Create variable  turns and second

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 var player1;
 var player2;
+
+function Players(roll,turn,score){
+	this.roll=roll;
+	this.turn= turn;
+	this.score= score;
+}
+var turns=[];
+var roll=0;
+var turn=0;
+var total= 0;
+var score1= 0;
+
+var turnsSecond=[];
+var rollSecond=0;
+var turnSecond=0;
+var totalSecond= 0;
+var score1Second= 0;
