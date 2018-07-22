d257ed43310d365b3fdd9843918a7999ea8f1e55
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-22 18:27:59 +0300

    Add constructors for players

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 var player1;
 var player2;
 
-var throwdice = function() {
+var dribble = function() {
   return Math.floor(Math.random() * 6) + 1;
+}
+
+function Player(turn) {
+  this.roll = 0;
+  this.myscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
 }
