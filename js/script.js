c9adfa304ee54842ad1f7fe058ed6da98f58081f
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-22 18:42:46 +0300

    Add prototypes for hold,findWinner and reset

--- js/script.js
+++ js/script.js
@@ -12,3 +12,30 @@ function Player(turn) {
   this.turn = turn;
   this.playerName;
 }
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+    this.myscore = 0;
+    alert("Ooops!! " + this.playerName + ", you rolled a one")
+
+  } else {
+    this.myscore += this.roll;
+  }
+}
+Player.prototype.hold = function() {
+  this.totalscore += this.myscore;
+  this.myscore = 0;
+  alert(this.playerName + ", your turn is over, pass the mouse!");
+}
+
+Player.prototype.winnerCheck = function() {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + " You are the winner!");
+  }
+}
+
+Player.prototype.reset = function() {
+  this.roll = 0;
+  this.myscore = 0;
+  this.totalscore = 0;
+  this.playerName = "";
+}
