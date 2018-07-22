abc9bc9d9b6b07e4b4adc7ed6678f1bb7e9c57bf
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-22 18:26:26 +0300

    Add variable dribble and open a function for it

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
 var player1;
 var player2;
 
-function Players(roll,turn,score){
-	this.roll=roll;
-	this.turn= turn;
-	this.score= score;
+var throwdice = function() {
+  return Math.floor(Math.random() * 6) + 1;
 }
-var turns=[];
-var roll=0;
-var turn=0;
-var total= 0;
-var score1= 0;
-
-var turnsSecond=[];
-var rollSecond=0;
-var turnSecond=0;
-var totalSecond= 0;
-var score1Second= 0;
-
-Players.prototype.rollOne= function(){
