40c4a6877a504d8c793740c891712f2ec67ca0b7
Nathan Ongoya <nathanongoya@gmail.com>
2018-07-22 19:05:25 +0300

    Add a variable to hold the input for player2 name

--- js/script.js
+++ js/script.js
@@ -45,6 +45,14 @@ $(document).ready(function() {
 	    player2 = new Player(false);
 	    $(".game-changer").show();
 	    $(".start-menu").hide();
-			
+
 			var player1Name = $(".player1Name").val();
 			    $("#player1Name").text(player1Name);
+
+					var player2Name = $(".player2Name").val();
+					    $("#player2Name").text(player2Name);
+
+					    player1.playerName = player1Name;
+					    player2.playerName = player2Name;
+
+					  });
